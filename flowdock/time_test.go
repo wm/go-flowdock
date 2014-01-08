dab256967b40b4acf668a99f09e65e17897ff705
Will Mernagh <wmernagh@gmail.com>
2014-01-08 15:39:56 +0100

    Test time

---
+++ flowdock/time_test.go
@@ -0,0 +1,20 @@
+package flowdock
+
+import (
+	"time"
+	"testing"
+)
+
+func TestTime_UnmarshalJSON(t *testing.T) {
+	flowdockTime := Time{}
+	json := []byte("1385546251160")
+	err := flowdockTime.UnmarshalJSON(json)
+	if err != nil {
+		t.Errorf("Time.UnmarshalJSON returned error: %v", err)
+	}
+
+	want := time.Date(2013, time.November, 27, 9, 57, 31, 0, time.UTC)
+	if flowdockTime.Local() != want.Local() {
+		t.Errorf("Time.UnmarshalJSON set time to %v, wanted %v", flowdockTime.Local(), want.Local())
+	}
+}
